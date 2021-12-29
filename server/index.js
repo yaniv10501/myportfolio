c9c5c45a5a5dcb75423ef4ae157b89a6f49d568d
Yaniv <yaniv10501@gmail.com>
2021-12-29 15:48:31 +0000

    .

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
@@ -18,7 +19,7 @@ app.set('env', NODE_ENV);
 app.use(helmet.contentSecurityPolicy({
   useDefaults: true,
   directives: {
-    "default-src": ["'self'", "https://yanivapi.com/email"],
+    "default-src": ["'self'"],
   },
 }));
 app.options('*', cors(corsOptions));
@@ -26,9 +27,16 @@ app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get('/test',  (req, res, next) => {
-  const ipOfSource = req.connection.remoteAddress;
-  res.send({ ip: ipOfSource, origin: req.header('Referer')});
+app.get('/test', (req, res, next) => {
+  const ipOfSource = req.socket.remoteAddress;
+  const origin = req.header('Referer'); 
+  fetch('https://yanivapi.com/email', {
+    headers: {
+    'Origin': origin,
+    },
+  }).then(async (res2) => {
+    res.send({ ip: ipOfSource, origin, res: await res2.json()});
+  });
 });
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
