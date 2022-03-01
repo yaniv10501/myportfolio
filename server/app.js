1231cf5293774d7eb9873a25aed243e72c45c1e6
Yaniv <yaniv10501@gmail.com>
2022-03-01 08:02:44 +0000

    Production

--- server/app.js
+++ server/app.js
@@ -35,10 +35,10 @@ app.use(cookieParser());
 
 app.use(requestLogger);
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 app.use(checkBots);
