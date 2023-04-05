2dd77501c1c205b56a0249b063c631e3fa0eb2cb
Yaniv Schweitzer <yaniv.s@jika.io>
2023-04-05 18:29:25 +0300

    try to fix google analytics

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,10 @@
 import React, { useCallback, useMemo } from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
+import ReactGa4 from 'react-ga4';
 import StyleContext from 'isomorphic-style-loader/StyleContext';
 import App from './components/App';
 
-window.dataLayer = window.dataLayer || [];
-function gtag(...args) {
-  window.dataLayer.push(args);
-}
-gtag('js', new Date());
-
-gtag('config', 'G-P9K37T374P');
+ReactGa4.initialize('G-P9K37T374P');
 
 let root;
 const domNode = document.getElementById('root');
